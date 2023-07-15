152d3f65684a4dbadbd7fe72da4f0dddae84923c
emymatista <emymatista05@gmail.com>
2023-07-15 16:31:13 -0400

    Juego Terminado

---
+++ Assets/Scripts/Core/WinningMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinningMenu : MonoBehaviour
+{
+   public void PlayGame()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
